df1eea31b08397414ae3aa9addf079c6bb70404f
Your Name <you@example.com>
2015-12-29 23:19:00 +0800

    Add functions to test

--- test/grid_unittest.cpp
+++ test/grid_unittest.cpp
@@ -1,6 +1,54 @@
 #include "grid.hpp"
 #include "gtest/gtest.h"
 
+// reset()
+TEST(XD, XD) {
+}
+
+// init()
+TEST(XD, XD) {                                                                  
+}
+
+// print()
+TEST(XD, XD) {                                                                  
+}
+
+// is_outside()
+TEST(XD, XD) {
+}
+
+// action()
+TEST(XD, XD) {
+}
+
+// has_empty()
+TEST(XD, XD) {
+}
+
+// get()
+TEST(XD, XD) {
+}
+
+// set()
+TEST(XD, XD) {
+}
+
+// largest()
+TEST(XD, XD) {
+}
+
+// move()
+TEST(XD, XD) {
+}
+
+// merge()
+TEST(XD, XD) {
+}
+
+// random_empty_pos()
+TEST(XD, XD) {
+}
+
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
